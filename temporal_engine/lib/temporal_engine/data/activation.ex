22e050b1b9e7e5d5e99a110935b8a7689918c3ae
Cory Finger <cory.j.finger@gmail.com>
2026-06-17 14:28:12 -0700

    Convert Activations to new Type DSL

--- temporal_engine/lib/temporal_engine/data/activation.ex
+++ temporal_engine/lib/temporal_engine/data/activation.ex
@@ -1,42 +1,67 @@
 defmodule TemporalEngine.Data.Activation do
-  require Record
+  use TemporalEngine.Data.TypeSpec
 
+  alias TemporalEngine.Data.Activation
   alias TemporalEngine.Data.Timestamp
   alias TemporalEngine.Data.Jobs
 
-  Record.defrecord(:activation, [
-    :run_id,
-    :is_replaying,
-    :history_length,
-    :jobs,
-    :available_internal_flags,
-    :history_size_bytes,
-    :continue_as_new_suggested,
-    :last_sdk_version,
-    :suggest_continue_as_new_reasons,
-    :target_worker_deployment_version_changed,
-    timestamp: nil,
-    deployment_version_for_current_task: nil
-  ])
+  deftype :activation do
+    @structdoc "An instruction to the lang sdk to run some workflow code, whether for the first time or from a cached state."
 
-  @type activation ::
-          record(:activation,
-            timestamp: Timestamp.timestamp(),
-            is_replaying: bool(),
-            history_length: pos_integer(),
-            jobs: [Jobs.job()],
-            available_internal_flags: [pos_integer()],
-            history_size_bytes: pos_integer(),
-            continue_as_new_suggested: bool(),
-            deployment_version_for_current_task: version(),
-            last_sdk_version: String.t(),
-            suggest_continue_as_new_reasons: [continue_as_new_reason()],
-            target_worker_deployment_version_changed: bool()
-          )
+    @doc """
+    The id of the currently active run of the workflow. Also used as a cache key.
+
+    There may only ever be one active workflow task (and hence activation) of a run at one time.
+    """
+    @type run_id :: required :: String.t()
+
+    @doc "The current time as understood by the workflow, which is set by workflow task started events"
+    @type timestamp :: Timestamp.timestamp()
+
+    @doc "Whether or not the activation is replaying past events"
+    @type is_replaying :: required :: bool()
+
+    @doc "Current history length as determined by the event id of the most recently processed event. This ensures that the number is always deterministic"
+    @type history_length :: required :: pos_integer()
+
+    @doc "The things to do upon activating the workflow"
+    @type jobs :: required :: [Jobs.job()]
+
+    @doc "Internal flags which are available for use by lang. If is_replaying is false, all internal flags may be used. This is not a delta - all previously used flags always appear since this representation is cheap."
+    @type available_internal_flags :: required :: [pos_integer()]
+
+    @doc "The history size in bytes as of the last WFT started event"
+    @type history_size_bytes :: pos_integer()
+
+    @doc "Set true if the most recent WFT started event had this suggestion"
+    @type continue_as_new_suggested :: bool()
+
+    @doc """
+    Set to the deployment version of the worker that processed this task, which may be empty.
+
+    During replay this version may not equal the version of the replaying worker.
+
+    If not replaying and this worker has a defined Deployment Version, it will equal that.
+
+    It will also be empty for evict-only activations. The deployment name may be empty, but not the build id, if this worker was using the deprecated Build ID-only feature(s).
+    """
+    @type deployment_version_for_current_task :: Activation.worker_deployment_version()
+
+    @doc "The last seen SDK version from the most recent WFT completed event"
+    @type last_sdk_version :: String.t()
+
+    @doc "Experimental. Optionally decide the versioning behavior that the first task of the new run should use. For example, choose to AutoUpgrade on continue-as-new instead of inheriting the pinned version of the previous run."
+    @type suggest_continue_as_new_reasons :: [Activation.continue_as_new_reason()]
+
+    @doc "Experimental. True if Workflow’s Target Worker Deployment Version is different from its Pinned Version and the workflow is Pinned."
+    @type target_worker_deployment_version_changed :: bool()
+  end
+
+  deftype :worker_deployment_version do
+    @type build_id :: required :: String.t()
+    @type deployment_name :: required :: String.t()
+  end
 
   @type continue_as_new_reason ::
           :unspecified | :history_size_too_large | :too_many_history_events | :too_many_updates
-
-  Record.defrecord(:version, [:build_id, :deployment_name])
-  @type version :: record(:version, build_id: String.t(), deployment_name: String.t())
 end
