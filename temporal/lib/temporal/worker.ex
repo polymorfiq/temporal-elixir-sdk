a7e26f73ff8fd87aff83dccb60de3a01ecb2bdfa
Cory Finger <cory.j.finger@gmail.com>
2026-06-18 14:10:38 -0700

    Start moving over to new Types

--- temporal/lib/temporal/worker.ex
+++ temporal/lib/temporal/worker.ex
@@ -3,6 +3,7 @@ defmodule Temporal.Worker do
 
   import TemporalEngine.Config
 
+  alias TemporalEngine.Config
   alias Temporal.Activity
   alias Temporal.Client
   alias Temporal.CoreSdk.CoreClient
@@ -19,226 +20,21 @@ defmodule Temporal.Worker do
 
   @type t :: %__MODULE__{id: String.t(), task_queue: TaskQueue.t()}
 
-  @supplier_schema {:or,
-                    [
-                      {:non_empty_keyword_list,
-                       [fixed_size: [required: true, type: :pos_integer]]},
-                      {:non_empty_keyword_list,
-                       [
-                         target_mem_usage: [required: true, type: :float],
-                         target_cpu_usage: [required: true, type: :float],
-                         min_slots: [required: true, type: :pos_integer],
-                         max_slots: [required: true, type: :pos_integer],
-                         ramp_throttle: [required: true, type: :float]
-                       ]}
-                    ]}
-
-  @tuner_schema [
-    workflow_slot_supplier: [required: true, type: @supplier_schema],
-    activity_slot_supplier: [required: true, type: @supplier_schema],
-    local_activity_slot_supplier: [required: true, type: @supplier_schema]
-  ]
-
-  @poller_schema {:or,
-                  [
-                    {:non_empty_keyword_list,
-                     [
-                       minimum: [required: true, type: :pos_integer],
-                       maximum: [required: true, type: :pos_integer],
-                       initial: [required: true, type: :pos_integer]
-                     ]},
-                    {:non_empty_keyword_list,
-                     [
-                       simple_maximum: [required: true, type: :pos_integer]
-                     ]}
-                  ]}
-
-  @opts_schema NimbleOptions.new!(
-                 namespace: [
-                   required: true,
-                   type: :string,
-                   doc: "The Temporal service namespace this worker is bound to"
-                 ],
-                 task_queue: [
-                   required: true,
-                   type: :string,
-                   doc:
-                     "What task queue will this worker poll from? This task queue name will be used for both workflow and activity polling."
-                 ],
-                 client_identity_override: [
-                   required: false,
-                   type: :string,
-                   doc:
-                     "A human-readable string that can identify this worker. Using something like sdk version and host name is a good default. If set, overrides the identity set (if any) on the client used by this worker."
-                 ],
-                 max_cached_workflows: [
-                   required: true,
-                   type: :pos_integer,
-                   doc:
-                     "If set nonzero, workflows will be cached and sticky task queues will be used, meaning that history updates are applied incrementally to suspended instances of workflow execution. Workflows are evicted according to a least-recently-used policy once the cache maximum is reached. Workflows may also be explicitly evicted at any time, or as a result of errors or failures."
-                 ],
-                 tuner: [
-                   required: true,
-                   doc:
-                     "Set a [WorkerTuner](https://docs.rs/temporalio-sdk-core/0.4.0/temporalio_sdk_core/trait.WorkerTuner.html) for this worker. Either this or at least one of the max_outstanding_* fields must be set",
-                   type: :non_empty_keyword_list,
-                   keys: @tuner_schema
-                 ],
-                 workflow_task_poller_behavior: [
-                   default: [simple_maximum: 5],
-                   type: @poller_schema,
-                   doc:
-                     "Maximum number of concurrent poll workflow task requests we will perform at a time on this worker’s task queue. See also [WorkerConfig::nonsticky_to_sticky_poll_ratio](https://docs.rs/temporalio-sdk-core/0.4.0/temporalio_sdk_core/struct.WorkerConfig.html#structfield.nonsticky_to_sticky_poll_ratio). If using SimpleMaximum, Must be at least 2 when max_cached_workflows > 0, or is an error."
-                 ],
-                 nonsticky_to_sticky_poll_ratio: [
-                   default: 0.2,
-                   type: :float,
-                   doc:
-                     "Only applies when using [PollerBehavior::SimpleMaximum](https://docs.rs/temporalio-sdk-core/0.4.0/temporalio_sdk_core/enum.PollerBehavior.html#variant.SimpleMaximum)
-
-                     (max workflow task polls * this number) = the number of max pollers that will be allowed for the nonsticky queue when sticky tasks are enabled. If both defaults are used, the sticky queue will allow 4 max pollers while the nonsticky queue will allow one. The minimum for either poller is 1, so if the maximum allowed is 1 and sticky queues are enabled, there will be 2 concurrent polls."
-                 ],
-                 activity_task_poller_behavior: [
-                   default: [simple_maximum: 5],
-                   type: @poller_schema,
-                   doc:
-                     "Maximum number of concurrent poll activity task requests we will perform at a time on this worker’s task queue"
-                 ],
-                 nexus_task_poller_behavior: [
-                   default: [simple_maximum: 5],
-                   type: @poller_schema,
-                   doc:
-                     "Maximum number of concurrent poll nexus task requests we will perform at a time on this worker’s task queue"
-                 ],
-                 task_types: [
-                   required: true,
-                   type: :non_empty_keyword_list,
-                   doc:
-                     "Specifies which task types this worker will poll for.
-
-                      Note: At least one task type must be specified or the worker will fail validation.",
-                   keys: [
-                     enable_workflows: [default: false, type: :boolean],
-                     enable_local_activities: [default: false, type: :boolean],
-                     enable_remote_activities: [default: false, type: :boolean],
-                     enable_nexus: [default: false, type: :boolean]
-                   ]
-                 ],
-                 sticky_queue_schedule_to_start_timeout: [
-                   default: {10, :seconds},
-                   type: {:tuple, [:pos_integer, {:in, [:seconds, :milliseconds]}]},
-                   type_doc:
-                     "[Duration.shorthand/0](`t:TemporalEngine.Data.Duration.shorthand/0`)",
-                   doc:
-                     "How long a workflow task is allowed to sit on the sticky queue before it is timed out and moved to the non-sticky queue where it may be picked up by any worker."
-                 ],
-                 max_heartbeat_throttle_interval: [
-                   default: {60, :seconds},
-                   type: {:tuple, [:pos_integer, {:in, [:seconds, :milliseconds]}]},
-                   type_doc:
-                     "[Duration.shorthand/0](`t:TemporalEngine.Data.Duration.shorthand/0`)",
-                   doc: "Longest interval for throttling activity heartbeats"
-                 ],
-                 default_heartbeat_throttle_interval: [
-                   default: {30, :seconds},
-                   type: {:tuple, [:pos_integer, {:in, [:seconds, :milliseconds]}]},
-                   type_doc:
-                     "[Duration.shorthand/0](`t:TemporalEngine.Data.Duration.shorthand/0`)",
-                   doc:
-                     "Default interval for throttling activity heartbeats in case ActivityOptions.heartbeat_timeout is unset. When the timeout is set in the ActivityOptions, throttling is set to heartbeat_timeout * 0.8"
-                 ],
-                 max_task_queue_activities_per_second: [
-                   required: false,
-                   type: :float,
-                   doc:
-                     "Sets the maximum number of activities per second the task queue will dispatch, controlled server-side. Note that this only takes effect upon an activity poll request. If multiple workers on the same queue have different values set, they will thrash with the last poller winning.
-
-                      Setting this to a nonzero value will also disable eager activity execution."
-                 ],
-                 max_worker_activities_per_second: [
-                   required: false,
-                   type: :float,
-                   doc:
-                     "Limits the number of activities per second that this worker will process. The worker will not poll for new activities if by doing so it might receive and execute an activity which would cause it to exceed this limit. Negative, zero, or NaN values will cause building the options to fail."
-                 ],
-                 ignore_evicts_on_shutdown: [
-                   default: false,
-                   type: :boolean,
-                   doc:
-                     "If set false (default), shutdown will not finish until all pending evictions have been issued and replied to. If set true shutdown will be considered complete when the only remaining work is pending evictions.
-
-                      This flag is useful during tests to avoid needing to deal with lots of uninteresting evictions during shutdown. Alternatively, if a lang implementation finds it easy to clean up during shutdown, setting this true saves some back-and-forth."
-                 ],
-                 graceful_shutdown_period: [
-                   required: false,
-                   type: {:tuple, [:pos_integer, {:in, [:seconds, :milliseconds]}]},
-                   type_doc:
-                     "[Duration.shorthand/0](`t:TemporalEngine.Data.Duration.shorthand/0`)",
-                   doc:
-                     "If set, core will issue cancels for all outstanding activities and nexus operations after shutdown has been initiated and this amount of time has elapsed"
-                 ],
-                 deployment_options: [
-                   required: true,
-                   type: :non_empty_keyword_list,
-                   keys: [
-                     version: [
-                       required: true,
-                       type: :non_empty_keyword_list,
-                       doc: "The deployment version of this worker",
-                       keys: [
-                         build_id: [required: true, type: :string],
-                         deployment_name: [required: true, type: :string]
-                       ]
-                     ],
-                     use_worker_versioning: [
-                       required: true,
-                       type: :boolean,
-                       doc:
-                         "If set, opts in to the Worker Deployment Versioning feature, meaning this worker will only receive tasks for workflows it claims to be compatible with."
-                     ],
-                     default_versioning_behavior: [
-                       required: false,
-                       type: {:in, [:unspecified, :pinned, :auto_upgrade, nil]},
-                       doc:
-                         "The default versioning behavior to use for workflows that do not pass one to Core. It is a startup-time error to specify :unspecified here."
-                     ]
-                   ]
-                 ],
-                 nondeterminism_as_workflow_fail: [
-                   default: true,
-                   type: :boolean,
-                   doc:
-                     "When enabled, Nondeterminism Errors cause any workflow being processed by this worker to fail, rather than simply failing the workflow task."
-                 ],
-                 nondeterminism_as_workflow_fail_for_types: [
-                   default: [],
-                   type: {:list, :string}
-                 ],
-                 plugins: [
-                   default: [],
-                   type: {:list, :string},
-                   doc: "List of plugins used by lang."
-                 ]
-               )
-
   @typedoc "Supported options:\n#{NimbleOptions.docs(@opts_schema)}"
-  @type worker_opts :: unquote(NimbleOptions.option_typespec(@opts_schema))
   @type extra_opts :: [{:forward_polled_messages, pid()}]
   @type task_queue :: String.t()
   @type activity_type :: String.t()
   @type register_workflow_opts :: [{:name, WorkflowName.t()}]
 
-  @spec new(TaskQueue.t(), worker_opts()) :: {:ok, t()} | {:error, term()}
+  @spec new(TaskQueue.t(), Config.worker_config()) :: {:ok, t()} | {:error, term()}
   def new(task_queue, opts \\ []) do
     opts = Keyword.put(opts, :task_queue, task_queue.queue_name)
     opts = Keyword.put_new(opts, :namespace, task_queue.client.namespace)
 
-    with {:ok, validated} <- NimbleOptions.validate(opts, @opts_schema) do
-      initialize_worker(task_queue, validated)
-    end
+    initialize_worker(task_queue, opts)
   end
 
-  @spec initialize_worker(TaskQueue.t(), worker_opts()) :: {:ok, t()} | {:error, term()}
+  @spec initialize_worker(TaskQueue.t(), Config.worker_config()) :: {:ok, t()} | {:error, term()}
   defp initialize_worker(task_queue, opts) do
     worker_id = Hash.random_hash(8)
     client = task_queue.client
